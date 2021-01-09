f6b82bd9b1aef849df0bdc729db41fe79ce5cd55
bec <becsun@icloud.com>
2021-01-09 21:20:30 +0000

    player and bunny choice works and showing the result

--- src/components/Bunnygame.js
+++ src/components/Bunnygame.js
@@ -8,11 +8,30 @@ class BunnyGame extends React.Component {
     result: '',
   }
 
-  choices = ['kale', 'peppers', 'celery', 'pellets' , 'parsley']
+  
 
   playGame = event => {
+    const choices = ['Kale', 'Peppers', 'Celery', 'Pellets' , 'Parsly']
     const playerChoice = event.target.value
+    const bunnyChoice = choices[Math.floor(Math.random() * choices.length)]
+    const result = this.results(playerChoice,bunnyChoice)
+    this.setState({
+      bunnyChoice,
+      playerChoice,
+      result,
+    })
+
     console.log(playerChoice)
+    console.log(bunnyChoice)
+    console.log(result)
+    console.log(playerChoice, bunnyChoice)
+  }
+
+  results = (playerChoice, bunnyChoice) => {
+    if (playerChoice === bunnyChoice) return 'bingo'
+    else {
+      return 'nice try'
+    }
   }
 
 
@@ -23,11 +42,26 @@ class BunnyGame extends React.Component {
       <>
         <p>bunnyChoice: {this.state.bunnyChoice} </p>
         <p>playerChoice: {this.state.playerChoice} </p>
-        <a className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Kale</a>
-        <a className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Peppers</a>
-        <a className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Celery</a>
-        <a className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Pellets</a>
-        <a className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Parsley</a>
+        <p>result: {this.state.result}</p>
+
+        <button onClick={this.playGame} 
+          value ="Kale" 
+          className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Kale</button>
+        <button onClick={this.playGame} 
+          value ="Peppers" 
+          className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Peppers</button>
+        <button onClick={this.playGame} 
+          value ="Celery" 
+          className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Celery</button>
+        <button onClick={this.playGame} 
+          value ="Pellets" 
+          className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Pellets</button>
+        <button onClick={this.playGame} 
+          value ="Parsley" 
+          className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Parsley</button>
+        <button onClick={this.playGame} 
+          value ="Hay" 
+          className="waves-effect waves-light btn"><i className="material-icons right">cloud</i>Hay</button>
       </>
     )
   }
