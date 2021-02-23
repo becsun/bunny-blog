236770dc2b9f4b75c619124cd6655fb2eec6e9b2
bec <becsun@icloud.com>
2021-02-23 23:21:11 +0000

    bunnies added to the page

--- src/components/BunnyBreeds.js
+++ src/components/BunnyBreeds.js
@@ -1,13 +1,8 @@
 // import axios from 'axios'
 import React from 'react'
+import BunnyBreedCard from './BunnyBreeds/BunnyBreedCard'
 import { getAllBunnies } from './lib/api'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
+
 
 
 class BunnyBreeds extends React.Component {
@@ -28,34 +23,16 @@ class BunnyBreeds extends React.Component {
     console.log('I have rendered', this.state.bunnies)
   
     return (
-      <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-              across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
+      <section>
+        <div className="container">
+          <div className="card-container">
+            {this.state.bunnies.map(bunny => (
+              <BunnyBreedCard key={bunny._id} {...bunny}/>
+            ))}
+          </div>
+        </div>
+      </section>
+      
     )
   }
 }
---
+++ src/components/BunnyBreeds/BunnyBreedCard.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+
+const BunnyBreedCard = ({ name, lifeSpan, origin, weight, furType, image, _id }) => (
+  <div>
+    <Link to={`/cheeses/${_id}`}>
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt={name}
+            height="160"
+            image={image}
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {lifeSpan}
+              {origin}
+              {weight}
+              {furType}
+              
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </Link>
+  </div>
+)
+  
+export default BunnyBreedCard
