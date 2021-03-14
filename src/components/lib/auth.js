e157cd116c5969fe6b9e8b2746b0f62933df77fe
bec <becsun@icloud.com>
2021-03-14 19:43:35 +0000

    token functions implemented to use for showing and hiding certain tab

--- src/components/lib/auth.js
+++ src/components/lib/auth.js
@@ -1,7 +1,26 @@
-// export const setToken = recievedToken => {
-//   window.localStorage.setItem('token', recievedToken)
-// }
+export const setToken = receivedToken => {
+  window.localStorage.setItem('token', receivedToken)
+}
 
-// export const getToken = () => {
-//   return window.localStorage.getOtem('token')
-// }
+export const getToken = () => {
+  return window.localStorage.getItem('token')
+}
+
+export const logout = () => {
+  window.localStorage.removeItem('token')
+}
+
+const getPayload = () => {
+  const token = getToken()
+  if (!token) return false
+  const parts = token.split('.')
+  if (parts.length < 3) return false
+  return JSON.parse(window.atob(parts[1]))
+}
+
+export const isAuthenticated = () => {
+  const payload = getPayload()
+  if (!payload) return false
+  const now = Math.round(Date.now() / 1000)
+  return now < payload.exp
+}
