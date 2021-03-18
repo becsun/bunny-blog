257b0117a190dea83808b310e97f5974147cfcf7
bec <becsun@icloud.com>
2021-03-18 20:14:13 +0000

    turn into a funtion page

--- src/components/Aboutpage.js
+++ src/components/Aboutpage.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-class Aboutpage extends React.Component {
-  render() {
+function Aboutpage() {
+ 
 
-    return (
-      <p>Hello world 3</p>
-    )
-  }
+  return (
+    <p>Hello world 3</p>
+  )
 }
+
 
 export default Aboutpage
